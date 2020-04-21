a894b184d0c9bb019836d3a1b2198e5f662425aa
JujubeH <thevortexdu8@gmail.com>
2020-04-21 17:10:10 +0200

    update

--- js/jeu/generer/generate.js
+++ js/jeu/generer/generate.js
@@ -4,3 +4,20 @@ function generer() {
 
     document.getElementById('affichage_mot').innerHTML = mots_aleatoire
 }
+
+function validation() {
+	let b =document.body;
+	var mots_user = document.getElementById("champ_entree").value;
+	var mots_aleatoire = document.getElementById('affichage_mot').innerHTML;
+		for (let i = 0; i < mots_aleatoire.length; i++) {
+  			if (mots_aleatoire[i].toLowerCase() == mots_user[i]) {
+				console.log(mots_aleatoire[i].toLowerCase());
+			} else if(mots_aleatoire[i].toLowerCase() != mots_user[i]) {
+				console.log(".");
+			}
+		}
+		if (mots_aleatoire.toLowerCase() == mots_user) {
+			alert("Win !");
+			}
+}
+
