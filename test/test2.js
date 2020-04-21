40de112b6f073c248cd474de74cad045667f1f07
Edouard <63109215+Alpha975@users.noreply.github.com>
2020-04-21 09:53:29 +0100

    udapte

---
+++ test/test2.js
@@ -0,0 +1,9 @@
+mot = ['CHIITE','MOTEUR','BATEAU','MIKADO','ANNEXE','TOMATE','BLEUET','MANOIR','RENARD','ZOMBIE'];
+/** 
+function generer() {
+    random = Math.ceil(Math.random()*9);
+    var mots_aleatoire = mot[random];
+
+    document.getElementById('affichage_mot').innerHTML
+}
+**/
