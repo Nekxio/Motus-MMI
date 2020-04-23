ac0465d6c56fe5ca8ca027d082f8a9085d44ba59
Nekxio <leohilaire.contact@gmail.com>
2020-04-23 16:34:59 +0200

    update

--- js/jeu/generer/generate.js
+++ js/jeu/generer/generate.js
@@ -5,18 +5,55 @@ function generer() {
     document.getElementById('affichage_mot').innerHTML = mots_aleatoire
 }
 
+/** 
+function majuscule() {
+	document.getElementById("champ_entree").this.value=this.value.toUpperCase();
+}
+**/
+
+
 function validation() {
-	let b =document.body;
 	var mots_user = document.getElementById("champ_entree").value;
 	var mots_aleatoire = document.getElementById('affichage_mot').innerHTML;
+
 		for (let i = 0; i < mots_aleatoire.length; i++) {
-  			if (mots_aleatoire[i].toLowerCase() == mots_user[i]) {
-				console.log(mots_aleatoire[i].toLowerCase());
-			} else if(mots_aleatoire[i].toLowerCase() != mots_user[i]) {
+  			if (mots_aleatoire[i] == mots_user[i]) {
+				console.log(mots_aleatoire[i]);
+
+			} else if(mots_aleatoire[i] != mots_user[i]) {
 				console.log(".");
+
+				document.getElementById('affichage_mot').style.color='#801b0f';
 			}
 		}
-		if (mots_aleatoire.toLowerCase() == mots_user) {
+
+		if (mots_aleatoire == mots_user) {
 			alert("Win !");
-			}
+
+			document.getElementById('mybutton').style.color='#3d9939';
+		}
 }
+
+
+
+
+/** 
+function choix(a,b) { //le a c'est pour this.value dans html, le b c'est le this.id dans le html
+    console.log(a,b);
+    
+    switch(a) {
+        case"Rouge": 
+            console.log('couleur1');
+            document.getElementById('lebouton').style.backgroundColor='#ff0000';
+            break;
+        case"Jaune":
+            console.log('couleur2');
+            document.getElementById('lebouton').style.backgroundColor='#f2ff00';
+            break;
+        case"Bleu":
+            console.log('couleur3');
+            document.getElementById('lebouton').style.backgroundColor='#1900ff';
+            break;
+    }
+}
+**/
