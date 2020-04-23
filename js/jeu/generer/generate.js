73fc102b7deeaf40d92c44bca9cc34efc25b4a36
Edouard <63109215+Alpha975@users.noreply.github.com>
2020-04-23 16:19:59 +0100

    udapte

--- js/jeu/generer/generate.js
+++ js/jeu/generer/generate.js
@@ -5,30 +5,33 @@ function generer() {
     document.getElementById('affichage_mot').innerHTML = mots_aleatoire
 }
 
-
+/** 
 function majuscule() {
-	document.getElementById('champ_entree').this.value.toUpperCase();
+	document.getElementById("champ_entree").this.value=this.value.toUpperCase();
 }
+**/
 
 
 function validation() {
-	let b =document.body;
 	var mots_user = document.getElementById("champ_entree").value;
 	var mots_aleatoire = document.getElementById('affichage_mot').innerHTML;
+
 		for (let i = 0; i < mots_aleatoire.length; i++) {
-  			if (mots_aleatoire[i].toUpperCase() == mots_user[i]) {
-				console.log(mots_aleatoire[i].toUpperCase());
-			} else if(mots_aleatoire[i].toUpperCase() != mots_user[i]) {
+  			if (mots_aleatoire[i] == mots_user[i]) {
+				console.log(mots_aleatoire[i]);
+
+			} else if(mots_aleatoire[i] != mots_user[i]) {
 				console.log(".");
 
-				document.getElementById('mybutton').style.color='#801b0f';
+				document.getElementById('affichage_mot').style.color='#801b0f';
 			}
 		}
-		if (mots_aleatoire.toUpperCase() == mots_user) {
+
+		if (mots_aleatoire == mots_user) {
 			alert("Win !");
 
 			document.getElementById('mybutton').style.color='#3d9939';
-			}
+		}
 }
 
 
