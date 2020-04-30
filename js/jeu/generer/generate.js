92ff28640ff85aa528aa4e15dd195a0872733735
Nekxio <leohilaire.contact@gmail.com>
2020-04-30 16:07:43 +0200

    update

--- js/jeu/generer/generate.js
+++ js/jeu/generer/generate.js
@@ -1,27 +1,20 @@
-function focus { 
-    document.getElementById('champ_entree').focus(); 
-}; 
-
-function generer() {
-    random = Math.ceil(Math.random()*199);
-    var mots_aleatoire = mot[random];
-
-    document.getElementById('affichage_mot').innerHTML = mots_aleatoire
-}
-
-/** 
-function majuscule() {
-	document.getElementById("champ_entree").this.value=this.value.toUpperCase();
-}
-**/
-
+function generer() { //Fonction permettant de prendre au hasard un mot dans le dictionnaire.js et d'en afficher 2 caractères
+    random = Math.ceil(Math.random()*199); //On stocke le code permettant la selection aléatoire d'un mot
+    var mots_aleatoire = mot[random]; //On l'applique au dictionnaire de mots
+    var caractere1 = mots_aleatoire.charAt(1); //Les variables caractere1 et caractere2 servent à prendre que le deuxième et et quatrième caractère du mot aléatoire
+    var caractere2 = mots_aleatoire.charAt(3);
+    document.getElementById("affichage_mot1").innerHTML = caractere1; //On affiche les 2 caractères du mot à la bonne place dans le tableau 
+    document.getElementById("affichage_mot3").innerHTML = caractere2;
+    
+    document.getElementById("affiche").innerHTML = mots_aleatoire;
+  }
 
 function validation() {
 	var mots_user = document.getElementById("champ_entree").value;
 	var mots_aleatoire = document.getElementById('affichage_mot').innerHTML;
 
 		for (let i = 0; i < mots_aleatoire.length; i++) {
-  			if (mots_aleatoire[i] == mots_user[i]) {
+  			if (mots_aleatoire.charAt[i] == mots_user.charAt[i]) {
 				console.log(mots_aleatoire[i]);
 
 			} else if(mots_aleatoire[i] != mots_user[i]) {
@@ -36,23 +29,4 @@ function validation() {
 
 			document.getElementById('mybutton').style.color='#3d9939';
 		}
-}
-
-function choix(a,b) { //le a c'est pour this.value dans html, le b c'est le this.id dans le html
-    console.log(a,b);
-    
-    switch(a) {
-        case"Rouge": 
-            console.log('couleur1');
-            document.getElementById('lebouton').style.backgroundColor='#ff0000';
-            break;
-        case"Jaune":
-            console.log('couleur2');
-            document.getElementById('lebouton').style.backgroundColor='#f2ff00';
-            break;
-        case"Bleu":
-            console.log('couleur3');
-            document.getElementById('lebouton').style.backgroundColor='#1900ff';
-            break;
-    }
 }
